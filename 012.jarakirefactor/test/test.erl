05f3996254c665d44cd9ee2460890ec2f2bb0c59
Jucimar Jr <jucimar.jr@gmail.com>
2012-03-14 13:39:01 -0400

    Acrescentado TUs para compilacao de todos os arquivos do java_src

--- 012.jarakirefactor/test/test.erl
+++ 012.jarakirefactor/test/test.erl
@@ -1,30 +1,14 @@
 -module(test).
 -include_lib("eunit/include/eunit.hrl").
 
-helloworld_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/J01Hello.java")).
+compile_all_test_() ->
+	{
+		"Jaraki compiling test...",
+		[compilar(JavaFile) || JavaFile <-	filelib:wildcard("java_src/*.java")]
+	}.
 
-media_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/J02Media.java")).
+compilar(JavaFile) ->
+	{filename:basename(JavaFile), [?_assertEqual(ok, jaraki:compile(JavaFile))]}.
 
-mediauea_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/J03MediaUea.java")).
 
-for7_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/TestandoFor7.java")).
-
-funcao22_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/funcao2.2.java")).
-
-listarprimos_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/ListarPrimos.java")).
-
-testandovar1_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/ATestandoVar01.java")).
-
-testandovar2_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/ATestandoVar02.java")).
-
-testandovar3_test_() ->
-	?_assertEqual(ok, jaraki:compile("java_src/ATestandoVar03.java")).
 
