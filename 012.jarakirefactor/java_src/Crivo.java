a5439831ce208fd9c4560eeb5a6e45aace97a691
Jucimar Jr <jucimar.jr@gmail.com>
2012-03-20 01:34:04 -0400

    DONE: movendo testes de recursao para djeiva/ ISSUE: nao esta parseando comentarios

--- 012.jarakirefactor/java_src/Crivo.java
+++ 012.jarakirefactor/java_src/Crivo.java
@@ -1,14 +1,35 @@
+/*
+ * testes de comentario
+ *
+ */
 public class Crivo {
 
 	public static void main(String[] args) {
 		int n, cont;
 		double maior;
 		cont = 0;
+	/*
+		double maior;
+		cont = 0;
+		n = 1000;
+		maior = Math.sqrt(n);
+*/
+
 		n = 1000;
 		maior = Math.sqrt(n);
 
+
+	/* int n, cont; */
+	/* 	double maior; */
+	/* 	cont = 0; */
+	/* 	n = 1000; */
+	/* 	maior = Math.sqrt(n); */
+
+
 		for(int i = 2; i <= n; i++){
 			cont  = 0;
+
+			// este eh um comentario
 
 			for(int j = 2; j <= maior; j++)
 				if (i != j)
@@ -16,6 +37,8 @@ public class Crivo {
 						cont = cont + 1;
 			if (cont == 0)
 				System.out.println(i);	
+			//if (cont == 0)
+			//	System.out.println(i);	
 		}
 	}
 }
--- 012.jarakirefactor/test/test.erl
+++ 012.jarakirefactor/test/test.erl
@@ -1,22 +1,21 @@
 -module(test).
 -include_lib("eunit/include/eunit.hrl").
 
-compile_all_test_() ->
-	{
-		"Jaraki compiling test...",
-		[compile_java(JavaFile) || JavaFile <- filelib:wildcard("java_src/*.java")]
-	}.
-
+%% compile_all_test_() ->
+%% 	{
+%% 		"Jaraki compiling test...",
+%% 		[compile_java(JavaFile) || JavaFile <- filelib:wildcard("java_src/*.java")]
+%% 	}.
+%% 
 compile_all_beam_test_() ->
 	{
-		"Jaraki generating .BEAM...",
+		"Jaraki compiling .JAVA and generating .BEAM...",
 		[compile_beam(JavaFile) || JavaFile <-	filelib:wildcard("java_src/*.java")]
 	}.
 
-compile_java(JavaFile) ->
-	{filename:basename(JavaFile), [?_assertEqual(ok, jaraki:compile(JavaFile))]}.
-
+%% compile_java(JavaFile) ->
+%% 	{filename:basename(JavaFile), [?_assertEqual(ok, jaraki:compile(JavaFile))]}.
+%% 
 compile_beam(JavaFile) ->
 	{filename:basename(JavaFile), [?_assertEqual(ok, jaraki:compile({beam,JavaFile}))]}.
-
 
