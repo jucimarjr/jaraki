36a272a350d7dea188c1749baa3aef63bb19f23d
Daniel Henrique <dhbaquino@gmail.com>
2012-03-21 15:00:41 -0400

    Melhorado o teste PF. Implementado dicionário de processo encapsulado.

---
+++ 012.jarakirefactor/pf/st.erl
@@ -0,0 +1,59 @@
+-module(st).
+-compile(export_all).	
+
+new() ->
+	ets:new(dict, [named_table]).
+
+destroy() ->
+	ets:delete(dict).
+
+%%SEMÂNTICA E GERAÇÃO DE CÓDIGO
+
+%% VarValue na análise semântica: undefined indica que não foi inicializada
+%% TODO: verificar se variável já existe...
+%% TODO: duas variáveis
+insert_var_list(Scope, [{identifier, VarName} | []], Type, VarValue) ->
+	get_declared(Scope, VarName, Type, VarValue),			
+	no_operation;
+
+insert_var_list(Scope, [{identifier, VarName} | Rest], Type, VarValue) ->
+	get_declared(Scope, VarName, Type, VarValue),	
+	insert_var_list(Scope, Rest, Type, VarValue).
+
+%% Semântica - Key = {Scope, VarName}, Value = {Type, VarValue}
+put(Key, Value) ->
+	ets:insert(dict, {Key, Value}).
+
+get(Scope, VarName) ->
+	VarValue = ets:lookup(dict, {Scope, VarName}),
+	[{_Key , {_Type, Value}}] = VarValue,
+	Value.
+
+delete(Scope, VarName) ->
+	ets:delete(dict, {Scope, VarName}).
+
+%% SEMÂNTICA
+get2(Scope, VarName) ->
+	VarValue = ets:lookup(dict, {Scope, VarName}),
+	case VarValue of
+		[{_Key , Value}] ->
+					Value;
+		[] ->
+			jaraki_exception:handle_error("Variable not declared")
+	end.
+
+get_declared(Scope, VarName, Type, VarValue) ->
+	case ets:lookup(dict, {Scope, VarName}) of
+		[] ->	
+			st:put({Scope, VarName}, {Type, VarValue});
+		[{_Key, _Value}] -> 
+			jaraki_exception:
+				handle_error("Variable already declared")
+	end.
+
+put_scope(Scope) ->
+	ets:insert(dict, {scope, Scope}).
+
+get_scope() ->
+	[{_Key, Scope}] = ets:lookup(dict, scope),
+	Scope.
