8eb1d090a61761e9ca6eddfe5be68650b4d842e6
Jucimar Jr <jucimar.jr@gmail.com>
2012-03-29 11:06:35 -0400

    melhorando o .gitignore e preparando testes para testar captura de erros

--- 012.jarakirefactor/test/test.erl
+++ 012.jarakirefactor/test/test.erl
@@ -7,15 +7,21 @@
 %% 		[compile_java(JavaFile) || JavaFile <- filelib:wildcard("java_src/*.java")]
 %% 	}.
 %% 
+
+%% compile_all_error_test_() ->
+%% 	{
+%% 		"Jaraki compiling .JAVA with errors...",
+%% 		[compile_error(JavaFile) || JavaFile <-	filelib:wildcard("java_erro/*.java")]
+%% 	}.
+%% 
 compile_all_beam_test_() ->
 	{
 		"Jaraki compiling .JAVA and generating .BEAM...",
 		[compile_beam(JavaFile) || JavaFile <-	filelib:wildcard("java_src/*.java")]
 	}.
 
-%% compile_java(JavaFile) ->
-%% 	{filename:basename(JavaFile), [?_assertEqual(ok, jaraki:compile(JavaFile))]}.
-%% 
 compile_beam(JavaFile) ->
 	{filename:basename(JavaFile), [?_assertEqual(ok, jaraki:compile({beam,JavaFile}))]}.
 
+%% compile_error(JavaFile) ->
+%% 	{filename:basename(JavaFile), [?_assertError(badarg, jaraki:compile({beam,JavaFile}))]}.
