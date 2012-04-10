d74e6ea3c823641cd4a6127540ee9c9ab180777f
Josiane Rodrigues <josi.enge@gmail.com>
2012-04-09 21:35:02 -0400

    Colocando arquivos .java na pasta java_src/

---
+++ 012.jarakirefactor/java_src/Repeticao5.java
@@ -0,0 +1,17 @@
+package repeticao;
+
+public class Repeticao5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 50; i++){
+			if(i%2 != 0)
+				System.out.println(i);					
+		}
+
+	}
+
+}
