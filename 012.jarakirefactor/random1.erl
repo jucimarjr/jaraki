941852348e7b59aac4f77048d4230e65c8ad96c8
Josiane Rodrigues <josi.enge@gmail.com>
2012-05-02 21:40:24 -0400

    Suporte ao Random

---
+++ 012.jarakirefactor/random1.erl
@@ -0,0 +1,38 @@
+-module(random1).
+
+-compile(export_all).
+
+-import(loop, [for/3, while/2]).
+
+-import(vector, [new/1, get_vector/1]).
+
+-import(randomLib, [function_random/2]).
+
+main(V_Args) ->
+    st:new(),
+    st:get_new_stack(main),
+    st:put_value({main, "Args"},
+		 {{array, 'String'}, V_Args}),
+    st:put_value({main, "randomInt"}, {int, trunc(6)}),
+    io:format("~s~n",
+	      ["Generating 10 random integers in range "
+	       "0..99."]),
+    begin
+      st:put_value({main, "idx"}, {int, 1}),
+      for(fun () -> st:get_value(main, "idx") =< 10 end,
+	  fun () ->
+		  st:put_value({main, "idx"},
+			       {int, trunc(st:get_value(main, "idx") + 1)})
+	  end,
+	  fun () ->
+		  st:put_value({main, "randomInt"},
+			       {int, trunc(function_random(next_int, 100))}),
+		  io:format("~s~p~n",
+			    ["Generated : ", st:get_value(main, "randomInt")])
+	  end),
+      st:delete(main, "idx")
+    end,
+    io:format("~s~n", ["Done."]),
+    st:get_old_stack(main),
+    st:destroy().
+
