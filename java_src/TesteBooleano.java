0fa0518110b0af15be5173bd0cc6073465683ad2
Helder e Lídia <ludus.labs.uea@gmail.com>
2012-05-14 20:26:27 -0400

    Teste com variáveis boolenas

---
+++ java_src/TesteBooleano.java
@@ -0,0 +1,14 @@
+public class TesteBooleano{
+  public static void main(String args[]){
+    boolean continuar = true;
+	boolean naoContinuar = false; 
+    
+    if(naoContinuar == true){
+      System.out.println("Podemos continuar.");
+    }
+    else{
+      System.out.println("Vamos parar.");
+    }    
+
+  }
+}
