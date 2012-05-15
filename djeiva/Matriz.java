471fcb0c9a557b59aa88f44cda2e5bfe061c1854
Lidia Lizziane <lidializz@gmail.com>
2012-05-14 22:50:30 -0400

    Matriz.java - Determinante por Laplace

--- djeiva/Matriz.java
+++ djeiva/Matriz.java
@@ -2,111 +2,50 @@ package matriz;
 
 import java.util.Random;
 
+
 public class Matriz {
-
-	private static double cofator(int x, int tam, int[][] Mat){
-		int p = 0;
-		for(int i = 1; i < tam; i++){
-			int s = 0;
-			for(int j=0; j<tam; j++){
-				if(j != x){
-					s++;
-					Mat[p][s] = Mat[i][j];
-				}
-			}
-		}
-		return (Math.pow(-1, 1 +x) * calcularDeterminante(p, Mat));
-	}
-
-
 	
-	private static int calcularDeterminante(int tam,int [][] Mat){
-		
-		int det = 0;
-		if(tam == 1){
-			return Mat[0][0];
-			
-		}else if (tam == 2){
-			det = Mat[0][0] * Mat[1][1] - Mat[0][1] * Mat[1][0];
-			return det;		
-		}else{
-			for(int i = 0; i<tam; i++){
-				if(Mat[0][i] != 0){
-					det += Mat[0][i] * cofator(i, tam, Mat);					
-				}
-			} 
-			
-			return det;
-		}
-	}
-	
-	
-	/*-----------------------------Codigo da net*------------------------------------------------/
-	/**
-	 * Calcula el determinante de la matriz. 
-	 * Para ello coge la primera fila y se va multiplicando cada coeficiente por el 
-	 * determinante de la matriz de orden n-1 que resulta de suprimir la fila y columna
-	 * del coeficiente. Hay que ir alternando los signos.
+	/* Determinante por laplace
+	 * Escolhe-se a primeira fila e multiplica-se cada coeficiente por um cofator = (-1)^(linha+coluna)
 	 * Ver http://www.marcevm.com/determinantes/determinantes_def.php
-	 * @param matriz
-	 * @return
 	 */
-	public static double determinante (int[][] mat)
-	{
-		assert mat != null;
-		assert mat.length>0;
-		assert mat.length == mat[0].length;
+	private static double calcularDeterminante(int [][] mat){
+		double det = 0.0;
+		int linhas = mat.length;
+		int colunas = mat[0].length;;
 		
-		double determinante = 0.0;
-		
-		int filas = mat.length;
-		int columnas = mat[0].length;
-		
-		// Si la matriz es 1x1, el determinante es el elemento de la matriz
-		if ((filas==1) && (columnas==1))
+		if(linhas == 1 && colunas ==1){
 			return mat[0][0];
-		
-
-		int signo=1;
-		
-		for (int columna=0;columna<columnas;columna++)
-		{
-			// Obtiene el adjunto de fila=0, columna=columna, pero sin el signo.
-			int[][] submatriz = getSubmatriz(mat, filas, columnas,
-					columna);
-			determinante = determinante + signo*mat[0][columna]*determinante(submatriz);
-			signo*=-1;
-		}
-		
-		return determinante;
+		}else{
+			int cofator = 1;
+			 for(int j=0; j<colunas; j++){
+				int[][] submatriz = getSubmatriz(mat, linhas, colunas, j);
+				det = det + cofator * mat[0][j] * calcularDeterminante(submatriz);
+				cofator*=-1;
+			}
+			 return det;
+		}	
 	}
-
-	/**
-	 * Obtiene la matriz que resulta de eliminar la primera fila y la columna que se
-	 * pasa como parámetro.
-	 * @param matriz Matriz original
-	 * @param filas Numero de filas de la matriz original
-	 * @param columnas Numero de columnas de la matriz original
-	 * @param columna Columna que se quiere eliminar, junto con la fila=0
-	 * @return Una matriz de N-1 x N-1 elementos
-	 */
-	public static int[][] getSubmatriz(int[][] matriz, 
-			int filas,
-			int columnas, 
-			int columna) {
-		int [][] submatriz = new int[filas-1][columnas-1];
+		
+	/* Matriz = matriz original
+	 * linhas e colunas da matriz original
+	 * a coluna que se quer eliminar junto com a linha 0
+	 * retorna uma matriz N-1 X N-1
+    */
+	public static int[][] getSubmatriz(int[][] matriz, int linhas, int colunas,	int coluna) {
+		int [][] submatriz = new int[linhas-1][colunas-1];
 		int contador=0;
-		for (int j=0;j<columnas;j++)
+		for (int j=0;j<colunas;j++)
 		{
-			if (j==columna) continue;
-			for (int i=1;i<filas;i++)
+			if (j==coluna) continue;
+			for (int i=1;i<linhas;i++)
 				submatriz[i-1][contador]=matriz[i][j];
 			contador++;
 		}
 		return submatriz;
 	}
 	
-	/*-----------------------------------------------------------------------------------------------*/
+
 	private	static void matrizTransposta(int[][] mat){
 		int aux = 0;
 		for(int i=0; i < mat.length; i++){
@@ -169,18 +108,12 @@ public class Matriz {
 		int[][] matD = new int[4][4];
 		int[][] mat = new int[4][4];
 		int[][] Mat = new int[3][3];
-		int[][] MatTeste = new int[3][3];
-		int tam = Mat.length;
-		int determ = 0;
-		
-		lerMatriz(Mat);
-		imprimirMatriz(Mat);
-		
-		System.out.println("O calculo do determinante" + calcularDeterminante(tam, Mat));
-		
+		int[][] MatTeste = new int[5][5];
+			
+				
 		lerMatriz(MatTeste);
 		imprimirMatriz(MatTeste);
-		System.out.println("O calculo do determinante net " + determinante(MatTeste));
+		System.out.println("O calculo do determinante net " + calcularDeterminante(MatTeste));
 		
 		lerMatriz(matB);
 		imprimirMatriz(matB);
